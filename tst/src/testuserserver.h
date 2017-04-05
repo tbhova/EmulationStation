158eef869623800174a3efa7db58aaf6b216bccd
Tyler Hovanec <tbhovanec@gmail.com>
2017-04-04 21:27:38 -0400

    Test run properply now using the GoogleTest configuration

---
+++ tst/src/testuserserver.h
@@ -0,0 +1,14 @@
+#ifndef EMULATIONSTATION_ALL_TESTUSERSERVER_H
+#define EMULATIONSTATION_ALL_TESTUSERSERVER_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
+using namespace std;
+
+TEST(TestSuite, TestCase) {
+    ASSERT_EQ(0,0);
+}
+
+#endif //EMULATIONSTATION_ALL_TESTUSERSERVER_H
