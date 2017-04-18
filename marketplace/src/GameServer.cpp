37c444b7e300ee2eb8eea61509ca622b04ddea22
Tyler Hovanec <tbhovanec@gmail.com>
2017-04-18 11:35:30 -0400

    Fixed spelling error

--- marketplace/src/GameServer.cpp
+++ marketplace/src/GameServer.cpp
@@ -1,17 +1,14 @@
 #include "GameServer.h"
 
-#include <grpc++/channel.h>
-
 using namespace std;
 
 using AvailableGameServer::GameDetails;
 using AvailableGameServer::GameFilters;
 using AvailableGameServer::GameId;
 using AvailableGameServer::GameIdList;
-using AvailableGameServer::AvialableGameServer;
 
 GameServer::GameServer(shared_ptr<grpc::Channel> channel)
-        : serverStub(AvialableGameServer::NewStub(channel)) {}
+        : serverStub(AvailableGameServer::AvailableGameServer::NewStub(channel)) {}
 
 vector<Game> GameServer::getDownloadableGames() {
     vector<Game> games = vector<Game>();
